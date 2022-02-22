7e6255fdfa5fa806a7e99500b91492549f5b70e7
ZukmanOliveira <58537010+ZukmanOliveira@users.noreply.github.com>
2022-02-21 23:09:51 -0300

    trainer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,5 +128,17 @@
                 </div>
             </div>
         </div>
+        <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  <label class="form-check-label" for="flexCheckDefault">
+    Default checkbox
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    Checked checkbox
+  </label>
+</div>
     </body>
 </html>
